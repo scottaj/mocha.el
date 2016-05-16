1d6d912b9dd9acd5ba76f8a710af61d2759e172f
Al Scott <al.scott@atomicobject.com>
2016-05-16 14:41:13 -0400

    Remove dir-locals from repository.

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (projectile-project-compilation-cmd . "cd /Users/ajs/projects/mocha-mode && make")
-  (projectile-project-test-cmd . "cd /Users/ajs/projects/mocha-mode && make test")))
