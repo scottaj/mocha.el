cbed12ceaaf946e33a8dfd0b7fbdcf6a3d019517
felipe <felipe@fov.space>
2017-03-20 20:25:24 +0100

    test the imenu integration

--- test/mocha-test.el
+++ test/mocha-test.el
@@ -120,3 +120,63 @@
                      "C:\\Users\\name\\node_modules\\jsdom\\lib\\jsdom\\browser\\Window.js"))
     (should (string= (match-string (nth 2 (car node-error-regexp-alist)) line) "477"))
     (should (string= (match-string (nth 3 (car node-error-regexp-alist)) line) "19"))))
+
+
+;;;; imenu integration
+
+(ert-deftest mocha-test/make-imenu-alist ()
+  (with-temp-buffer
+    (insert "
+describe('something', function() {
+  beforeEach(() => console.log('sometihng'));
+  it(\"does something\", () => {});
+  describe(\"on the inside\", function() {
+    beforeAll(setupFunction);
+    afterEach(partialReset);
+    it(\"does some more, but not tested\");
+    features.forEach(feat => {
+        it(\"feature test: \" + feat, () => {
+          // ...
+        })
+    });
+    afterAll(totalReset);
+  });
+});
+
+describe(\"another top-level\", () => {});
+afterAll(() => {});")
+    (js2-mode)
+    (js2-parse)
+    (require 'mocha)
+    (let (imenu-max-item-length)
+      (should (equal
+               (mocha-make-imenu-alist)
+               '(("describe something" .
+                  (("*declaration*" . 2)
+                   ("beforeEach" . 39)
+                   ("it does something" . 85)
+                   ("describe on the inside" .
+                    (("*declaration*" . 119)
+                     ("beforeAll" . 162)
+                     ("afterEach" . 192)
+                     ("it does some more, but not tested" . 221)
+                     ("it \"feature test: \" + feat" . 298)
+                     ("afterAll" . 374)))))
+                 ("describe another top-level" .
+                  (("*declaration*" . 407)))
+                 ("afterAll" . 448)))))))
+
+(ert-deftest mocha-test/toggle-imenu-function ()
+  (with-temp-buffer
+    (insert "function setUp() {}\ndescribe(\"top-level\", () => {it('works');});")
+    (js2-mode)
+    (js2-parse)
+    (let ((prev (imenu--make-index-alist)))
+      (mocha-toggle-imenu-function)
+      (should (equal (imenu--make-index-alist)
+                     '(("*Rescan*" . -99)
+                       ("describe top-level"
+                        ("*declaration*" . 21)
+                        ("it works" . 50)))))
+      (mocha-toggle-imenu-function)
+      (should (equal (imenu--make-index-alist) prev)))))
