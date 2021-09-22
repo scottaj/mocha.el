8fa3a54ac68302868ad60737a9e5662b85a511b0
Alhadis <gardnerjohng@gmail.com>
2021-09-22 18:50:36 +1000

    Tell `js2-mode` about Mocha's globals

--- mocha.el
+++ mocha.el
@@ -53,6 +53,59 @@
   :group 'mocha
   :safe #'stringp)
 
+(defcustom mocha-test-directory-regexp
+   "/\\(test\\|spec\\)s?/?$"
+   "Regular expression for identifying test directories."
+   :type 'string
+   :group 'mocha)
+
+(defconst mocha-bdd-globals
+  '("after"
+    "afterEach"
+    "before"
+    "beforeEach"
+    "context"
+    "describe"
+    "it"
+    "specify")
+  "Functions globalised by Mocha's `BDD' interface.")
+
+(defconst mocha-bdd-globals
+  '("after"
+    "afterEach"
+    "before"
+    "beforeEach"
+    "context"
+    "describe"
+    "it"
+    "specify")
+  "Functions globalised by Mocha's `BDD' interface.")
+
+(defconst mocha-tdd-globals
+  '("setup"
+    "suite"
+    "suiteSetup"
+    "suiteTeardown"
+    "test"
+    "teardown")
+  "Functions globalised by Mocha's `TDD' interface.")
+
+(defconst mocha-qunit-globals
+  '("after"
+    "afterEach"
+    "before"
+    "beforeEach"
+    "test"
+    "suite")
+  "Functions globalised by Mocha's `QUnit' interface.")
+
+(add-hook 'js2-init-hook
+   (lambda ()
+      (when (and (stringp (buffer-file-name))
+                 (string-match mocha-test-directory-regexp
+                 (file-name-directory (buffer-file-name))))
+      (setq js2-additional-externs mocha-bdd-globals))))
+
 (defcustom mocha-debugger 'realgud
   "Which debugger to use."
   :type '(choice (const :tag "realgud" realgud)
